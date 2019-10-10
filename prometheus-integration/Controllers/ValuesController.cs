8b6c28fb4c564e4b875fc22bb6c9c98b38a43596
Alex Pirtea <pirtea.a@gmail.com>
2019-10-10 10:53:13 +0300

    initial commit

---
+++ prometheus-integration/Controllers/ValuesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace prometheus_integration.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private Random r = new Random();
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> Get()
+        {
+            // assign some SOH memory
+            var x = new byte[1024];
+
+            // assign some LOH memory
+            x = new byte[1024 * 100];
+
+            // await a task (will result in a Task being scheduled on the thread pool) 
+            await Task.Yield();
+
+            var val = this.r.Next();
+            CompileMe(() => val);
+
+            return new string[] { "value1" + this.r.Next(), "value2" + this.r.Next() };
+        }
+
+        private void CompileMe(Expression<Func<int>> func)
+        {
+            func.Compile()();
+        }
+    }
+}
---
+++ prometheus-integration/Program.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Prometheus.DotNetRuntime;
+
+namespace prometheus_integration
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DotNetRuntimeStatsBuilder.Customize()
+                   .WithThreadPoolSchedulingStats()
+                   .WithContentionStats()
+                   .WithGcStats()
+                   .WithJitStats()
+                   .WithThreadPoolStats()
+                   .WithErrorHandler(ex => Console.WriteLine("ERROR: " + ex.ToString()))
+                   //.WithDebuggingMetrics(true);
+                   .StartCollecting();
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseKestrel()
+                              .UseUrls("http://*:5090")  ;
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
